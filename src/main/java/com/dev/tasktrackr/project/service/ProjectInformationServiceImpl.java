8a506325729a3660cbc441abd07e40e1fde21f54
Dxme98 <dominik-bleser@web.de>
2025-09-25 16:11:22 +0200

    added permissions to informationservice

--- src/main/java/com/dev/tasktrackr/project/service/ProjectInformationServiceImpl.java
+++ src/main/java/com/dev/tasktrackr/project/service/ProjectInformationServiceImpl.java
@@ -2,10 +2,7 @@ package com.dev.tasktrackr.project.service;
 
 import com.dev.tasktrackr.project.api.dtos.request.CreateLinkRequest;
 import com.dev.tasktrackr.project.api.dtos.request.UpdateInformationContentRequest;
-import com.dev.tasktrackr.project.domain.BasicDetails;
-import com.dev.tasktrackr.project.domain.Information;
-import com.dev.tasktrackr.project.domain.Link;
-import com.dev.tasktrackr.project.domain.Project;
+import com.dev.tasktrackr.project.domain.*;
 import com.dev.tasktrackr.project.repository.ProjectRepository;
 import com.dev.tasktrackr.shared.exception.custom.NotFoundExceptions.ProjectNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +20,8 @@ public class ProjectInformationServiceImpl implements ProjectInformationService 
     @Transactional
     public Information updateContent(Long projectId, String jwtUserId, UpdateInformationContentRequest updateInformationContentRequest) {
         Project project =  findProjectById(projectId);
+        memberHasPermission(jwtUserId, project);
+
         BasicDetails basicDetails = project.getBasicDetails();
 
         Information updatedInformation = basicDetails.updateInformationContent(updateInformationContentRequest);
@@ -36,6 +35,8 @@ public class ProjectInformationServiceImpl implements ProjectInformationService 
     @Transactional
     public Link addLink(Long projectId, String jwtUserId, CreateLinkRequest createLinkRequest) {
         Project project = findProjectById(projectId);
+        memberHasPermission(jwtUserId, project);
+
         BasicDetails basicDetails = project.getBasicDetails();
 
         Link addedLink = basicDetails.addLink(createLinkRequest);
@@ -50,29 +51,38 @@ public class ProjectInformationServiceImpl implements ProjectInformationService 
     @Transactional
     public void deleteLink(Long projectId, String jwtUserId, Long linkId) {
         Project project = findProjectById(projectId);
+        memberHasPermission(jwtUserId, project);
         BasicDetails basicDetails = project.getBasicDetails();
 
         basicDetails.deleteLink(linkId);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Set<Link> findLinksByProjectId(Long projectId, String jwtUserId) {
         Project project = findProjectById(projectId);
+        project.isProjectMember(jwtUserId);
         BasicDetails basicDetails = project.getBasicDetails();
 
         return basicDetails.getLinks();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Information findContentByProjectId(Long projectId, String jwtUserId) {
         Project project = findProjectById(projectId);
+        project.isProjectMember(jwtUserId);
         BasicDetails basicDetails = project.getBasicDetails();
 
         return basicDetails.getInformation();
     }
-
     private Project findProjectById(Long projectId) {
         return projectRepository.findById(projectId)
                 .orElseThrow(() -> new ProjectNotFoundException(projectId));
+    }
+
+    private void memberHasPermission(String jwtUserId, Project project) {
+        ProjectMember member = project.findProjectMember(jwtUserId);
+        member.canEditInformation();
     }
 }
